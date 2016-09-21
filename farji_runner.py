2a02a134e8965e5f7cd8b187db4d2c40f934b527
Jesse <jesse@practo.com>
2016-09-21 23:25:20 +0530

    changed indentation to spaces

--- farji_runner.py
+++ farji_runner.py
@@ -10,64 +10,64 @@ test_folder = sys.argv.pop()
 options = sys.argv[1:]
 
 if '-d' in options:
-	ind = options.index('-d')
-	ind1 = command.index('-d')
-	options.pop(ind)
-	results_folder = options.pop(ind)
-	command.pop(ind1)
-	command.pop(ind1)
+    ind = options.index('-d')
+    ind1 = command.index('-d')
+    options.pop(ind)
+    results_folder = options.pop(ind)
+    command.pop(ind1)
+    command.pop(ind1)
 
 command = ' '.join(command)
 options = ' '.join(options)
 
 
 def run_tests(command, test_folder, options, results_folder):
-	if command:
-		suites = initiate_dry_run(command)
-		job = TaskSet(tasks=[execute_test.subtask(kwargs={'path': suite, 'test_path': test_folder, 'options': options, 'results_folder': results_folder}) for suite in suites])
-		print '#########################################################'
-		print 'Executing Tests'
-		print '#########################################################'
-		result = job.apply_async()
+    if command:
+        suites = initiate_dry_run(command)
+        job = TaskSet(tasks=[execute_test.subtask(kwargs={'path': suite, 'test_path': test_folder, 'options': options, 'results_folder': results_folder}) for suite in suites])
+        print '#########################################################'
+        print 'Executing Tests'
+        print '#########################################################'
+        result = job.apply_async()
 
-		while not result.ready():
-			time.sleep(0.1)
+        while not result.ready():
+            time.sleep(0.1)
 
-		print '#########################################################'
-		print 'Finished tests, generating report'
-		print '#########################################################'
-		merge_results(suites, test_folder, results_folder)		
+        print '#########################################################'
+        print 'Finished tests, generating report'
+        print '#########################################################'
+        merge_results(suites, test_folder, results_folder)      
 
-	else:
-		raise ValueError('No test folder provided')
+    else:
+        raise ValueError('No test folder provided')
 
 
 def initiate_dry_run(command):
-	FNULL = open(os.devnull, 'w')
-	subprocess.call("pybot --dryrun --output=suites.xml --report=NONE --log=NONE %s" % command, stdout=FNULL, stderr=subprocess.STDOUT, shell=True)
-	tree = ET.parse('suites.xml')
-	root = tree.getroot()
-	suites = []
-	for suite in root.iter('suite'):
-		attrs = suite.attrib
-		if 'source' in attrs:
-		 	path = attrs['source']
-		 	if '.robot' in path:
-		 		suites.append(path)
-	os.remove('suites.xml')
-	return suites
+    FNULL = open(os.devnull, 'w')
+    subprocess.call("pybot --dryrun --output=suites.xml --report=NONE --log=NONE %s" % command, stdout=FNULL, stderr=subprocess.STDOUT, shell=True)
+    tree = ET.parse('suites.xml')
+    root = tree.getroot()
+    suites = []
+    for suite in root.iter('suite'):
+        attrs = suite.attrib
+        if 'source' in attrs:
+            path = attrs['source']
+            if '.robot' in path:
+                suites.append(path)
+    os.remove('suites.xml')
+    return suites
 
 
 def merge_results(suites, test_folder, results_folder):
-	for i in range(len(suites)):
-		suite = suites[i]
-		index = suite.rfind(test_folder)
-		new_suite = suite[:index]
-		test_path = suite[index:]
-		folder = string.replace(test_path, '/', '.')
-		suites[i] = new_suite + results_folder + '/' + folder + '/result.xml'
-		output_directory = new_suite + results_folder
-	subprocess.call('rebot --outputdir=%s --name=Tests --output output.xml %s' % (output_directory, ' '.join(suites)), shell=True)
+    for i in range(len(suites)):
+        suite = suites[i]
+        index = suite.rfind(test_folder)
+        new_suite = suite[:index]
+        test_path = suite[index:]
+        folder = string.replace(test_path, '/', '.')
+        suites[i] = new_suite + results_folder + '/' + folder + '/result.xml'
+        output_directory = new_suite + results_folder
+    subprocess.call('rebot --outputdir=%s --name=Tests --output output.xml %s' % (output_directory, ' '.join(suites)), shell=True)
 
 
 
--- tasks.py
+++ tasks.py
@@ -8,16 +8,16 @@ logger = get_task_logger(__name__)
 
 @app.task()
 def execute_test(**kwargs):
-	path = kwargs.get('path')
-	folder_name = kwargs.get('test_path')
-	command = kwargs.get('options')
-	results_folder = kwargs.get('results_folder')
-	index = path.rfind(folder_name)
-	new_path = path[:index]
-	test_path = path[index:]
-	folder = string.replace(test_path, '/', '.')
-	output_path = new_path + results_folder + '/' + folder
-	logger.info('Executing Test %s' % folder)
-	FNULL = open(os.devnull, 'w')
-	subprocess.call('pybot --outputdir=%s --output=result.xml --report=NONE --log=NONE %s %s' % (output_path, command, path), stdout=FNULL, stderr=subprocess.STDOUT, shell=True)
-	logger.info('Ended Test %s' % folder)
+    path = kwargs.get('path')
+    folder_name = kwargs.get('test_path')
+    command = kwargs.get('options')
+    results_folder = kwargs.get('results_folder')
+    index = path.rfind(folder_name)
+    new_path = path[:index]
+    test_path = path[index:]
+    folder = string.replace(test_path, '/', '.')
+    output_path = new_path + results_folder + '/' + folder
+    logger.info('Executing Test %s' % folder)
+    FNULL = open(os.devnull, 'w')
+    subprocess.call('pybot --outputdir=%s --output=result.xml --report=NONE --log=NONE %s %s' % (output_path, command, path), stdout=FNULL, stderr=subprocess.STDOUT, shell=True)
+    logger.info('Ended Test %s' % folder)
